d874f5f6e1f58a40762c923ed40f1d24a9dd9de1
Joshua Rand <JRand@student.dwc.edu>
2014-12-04 16:02:00 -0500

    Added sound player

--- Minesweeper/MyForm.h
+++ Minesweeper/MyForm.h
@@ -14,6 +14,7 @@ namespace Minesweeper {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Media;
 
 	const int TILE_WIDTH = 21;
 	const int TILE_HEIGHT = 20;
@@ -91,6 +92,7 @@ namespace Minesweeper {
 		Bitmap^ resetButtonClicked = gcnew Bitmap("graphicsformine/resetbutton/resetclicked.png");
 		Tile* tiles;
 		ResetButton* myResetButton;
+		SoundPlayer ^player;
 		
 		//Resets the tile field for when the user restarts the game
 		void ResetField(){
@@ -371,6 +373,9 @@ namespace Minesweeper {
 				 gameOver = false;
 				 CreateTiles();
 				 myResetButton = new ResetButton((panelWidth / 2) - (resetButton->Width / 2), (panelHeight - resetButton->Height));
+				 player = gcnew System::Media::SoundPlayer();
+				 player->SoundLocation = "jeopardy.wav";
+				 player->Play();
 	}
 	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
 				// g->DrawImage(bmp2, 0, 0, 37, 36);
